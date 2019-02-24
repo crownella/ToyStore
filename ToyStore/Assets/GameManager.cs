64ec2beffe996a43279b4d2112cd91982a793224
K8 <kah703@nyu.edu>
2019-02-24 17:21:35 -0500

    Intial Commit

---
+++ ToyStore/Assets/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public bool driving = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (driving == true)
+            {
+                SceneManager.LoadScene("MainScene");
+                driving = false;
+            }
+            else
+            {
+                SceneManager.LoadScene("Driving");
+                driving = true;
+            }
+        }
+        
+        
+    }
+}
---
+++ ToyStore/Assets/Player_Controller.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+    private Rigidbody rb;
+    public Camera cam;
+    private float pitch; //mouse up/down
+    private float yaw;//mouse left/right
+    private float fpForback; //inputfloat w/s
+    private float fpStrafe;//inputfloat a/d
+    private Vector3 inputVelocity; //cumilative vel to move character
+    public float velocityMod; //veloctiy timer this modifier
+    public float sensX;
+    public float sensY;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        yaw = Input.GetAxis("Mouse X") * sensX;
+        transform.Rotate(0,yaw,0);
+
+        pitch = Input.GetAxis(("Mouse Y")) * sensY;
+        cam.transform.Rotate(-pitch,0,0);
+
+        fpForback = Input.GetAxis("Vertical");
+        fpStrafe = Input.GetAxis("Horizontal");
+
+        inputVelocity = transform.forward * fpForback;
+        inputVelocity += transform.right * fpStrafe;
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = inputVelocity * velocityMod + (Physics.gravity * .69f);
+    }
+}
